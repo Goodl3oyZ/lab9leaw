1016325139ca39182205d0f010a12dcfffe14a26
Goodl3oyZ <panudetzx2@gmail.com>
2022-12-23 16:22:25 +0700

    sed krub

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,13 +1,48 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+int main()
+{
+    int age,height,bounty;
+    string cha;
+    cout<<"Enter your age: ";
+    cin>>age;
+    if(age<=25){
+        cout<<"Enter your height: ";
+        cin>>height;
+        if(height<100)
+        {
+            cha="Chopper";
+        }else if(height<180)
+        {
+            cha="Usopp";
+        }else
+        {
+            cout<<"Enter your bounty: ";
+            cin>>bounty;
+        if(bounty>1.1*1e9)
+            {
+            cha="Zoro";
+        }else
+            {
+            cha="Sanji";
+            }
+        }
+    }else if(age<=60)
+    {
+        cout<<"Enter your bounty: ";
+        cin>>bounty;
+        if(bounty>5*1e8)
+        {
+            cha="Jinbe";
+        }else
+        {
+            cha="Franky";
+        }
+    }else
+    {
+        cha="Brook";
+    }
+    cout<<"Your character = "<<cha;
+}
